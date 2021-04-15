8b3dd20c0d51080e58c89cfd492f9ba0dda21d75
yun min seop <201411yms@dsm.hs.kr>
2021-04-15 19:48:26 +0900

    2.0end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
  
 import React from "react";
+import Potato from "./potato";
 function App() {
-  return <div><h1>hello</h1></div>
-
+  return <div><h1>hello</h1>
+  <Potato/>
+  </div>
 }
 
 export default App;
---
+++ src/potato.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Potato() {
+    return <h3>i love potato</h3> ;
+}
+
+export default Potato;
